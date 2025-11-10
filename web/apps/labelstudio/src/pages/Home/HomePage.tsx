e0d32b96d8d7220774223acd61e6a61a8c29218c
monishallada <monishallada@gmail.com>
2025-11-10 13:38:18 -0500

    asfadfasdadasdasdsasdcasdaq

--- web/apps/labelstudio/src/pages/Home/HomePage.tsx
+++ web/apps/labelstudio/src/pages/Home/HomePage.tsx
@@ -176,7 +176,7 @@ export const HomePage: Page = () => {
           </SimpleCard>
           <div className="flex gap-2 items-center">
             <IconHumanSignal />
-            <span className="text-neutral-content-subtle">Label Studio Version: Community</span>
+            <span className="text-neutral-content-subtle">Microknots Version: Community</span>
           </div>
         </section>
       </div>
