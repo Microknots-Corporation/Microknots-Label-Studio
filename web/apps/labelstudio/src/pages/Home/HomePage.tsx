4cfcb7491b7c5757a2b8360e0dfce09a0c3ed4de
monishallada <monishallada@gmail.com>
2025-12-04 14:51:14 -0500

    akjdefhgiou;qaefhg;eqougfrquei;awgfeiuswfgew;l.iku

--- web/apps/labelstudio/src/pages/Home/HomePage.tsx
+++ web/apps/labelstudio/src/pages/Home/HomePage.tsx
@@ -84,7 +84,7 @@ export const HomePage: Page = () => {
         <section className="flex flex-col gap-6">
           <div className="flex flex-col gap-1">
             <Typography variant="headline" size="small">
-               Aurelo Data Engine 👋
+               Aurelo Data Engine Foundation Model👋
             </Typography>
             <Typography size="small" className="text-neutral-content-subtler">
               Developed by Microknots Corporation
